efbe856383fad85a6aa2533c611f59444f407ed8
kamelBOUSSAAD <kamel.boussaad@gmail.com>
2018-12-17 18:27:41 +0100

    BACKEND API ROUTER user & Login

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports ={
-    mongoURI : 'mongodb://kamel:ktabi3200@ds129386.mlab.com:29386/ktabi'
+    mongoURI : 'mongodb://kamel:ktabi3200@ds129386.mlab.com:29386/ktabi',
+    secretOrKey: 'secretkamel'
 }
--- server.js
+++ server.js
@@ -1,39 +1,38 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyPareser = require('body-parser')
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
+const users = require('./routes/api/users');
+// const profile = require('./routes/api/profile');
+// const posts = require('./routes/api/posts');
 
 const app = express();
 
-//bodyParser middleware 
-// for access to the body for ever 
-app.use(bodyPareser.urlencoded({extended : false}))
-app.use(bodyPareser.json())
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+// DB Config
+const db = require('./config/keys').mongoURI;
 
-//db config 
-const db = require ('./config/keys').mongoURI;
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
+// Passport middleware
+app.use(passport.initialize());
 
-const users = require('./routes/api/users');
-const profiel = require('./routes/api/profiel');
-// const admin = require('./routes/api/admin');
-// const book = require('./routes/api/book');
+// Passport Config
+require('./config/passport')(passport);
 
-// connect to mongodb
-//then && catch is promise in js 
-mongoose.connect(db)
-  .then(() => console.log('mongoDB connected '))
-.catch((err)=>console.log('Fail connected to database'))
-app.get('/', (req,res)=>res.send('hello'));
-
-//user routes
+// Use Routes
 app.use('/api/users', users);
-app.use('/api/profiel', profiel);
-// app.use('/api/admin', admin);
-// app.use('/api/book', book);
+// app.use('/api/profile', profile);
+// app.use('/api/posts', posts);
 
+const port = process.env.PORT || 5000;
 
-
-const port =  process.env.PORT || 5000;
-app.listen(port , () => console.log(`server runing on port ${port}`))
+app.listen(port, () => console.log(`Server running on port ${port}`));
